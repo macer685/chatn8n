b4987f6c2446e294893ab59262760f36fb9f4734
macer685 <macercreative@gmail.com>
2025-04-04 23:30:58 -0500

    Update script.js
    usuario

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ async function recuperarChats() {
     const response = await fetch(recuperarChatsUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ usuario: userId })
+      body: JSON.stringify({ userId })
     });
     if (!response.ok) {
       throw new Error(`Error al recuperar chats: ${response.statusText}`);
